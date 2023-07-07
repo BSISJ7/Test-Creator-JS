e4d807b6f257a240e79cd73c4dc1eca66ec4db8c
BSISJ7 <25395733+BSISJ7@users.noreply.github.com>
2023-07-07 16:29:50 -0400

    Checkbox questions have been added.
    -checkbox questions added
    -changed how answer div displays questions its height now scales with number of questions
    -arrow keys can be used to go to previous or next question

--- Note Card.js
+++ Note Card.js
@@ -3,10 +3,30 @@ const print = printValue => {console.log(printValue)};
 const randNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 const TEST_QUESTIONS = [];
 const ANSWERS_DIV = document.getElementById('answersDiv');
+const QUESTION_TYPES = {
+    MULTIPLE_CHOICE: 'mult',
+    CHECK_BOX: 'check',
+    FILL_IN_THE_BLANK: 'fill'
+}
+Object.freeze(QUESTION_TYPES);
 
 let questionIndex = 0; //Currently displayed question
 let testSubmitted = false;
 let shuffleQuestions = true;
+
+
+//Go to previous or next question using arrow keys
+document.onkeydown = (e) =>{
+    e = e || window.event;
+    if (e.keyCode == '37' && TEST_QUESTIONS.length > 1) {
+        questionIndex = questionIndex > 0 ? questionIndex-1 : TEST_QUESTIONS.length-1;
+        loadQuestion();
+    }
+    else if (e.keyCode == '39' && TEST_QUESTIONS.length > 1) {
+        questionIndex = questionIndex < TEST_QUESTIONS.length-1 ? questionIndex+1 : 0;
+        loadQuestion();
+    }
+};
 
 
 document.getElementById('testInput').onchange = () =>{
@@ -38,10 +58,7 @@ document.getElementById("beginTestBtn").onclick = () =>{
     
     testSubmitted = false;
     questionIndex = 0;
-        
-    for(let x = 0; x < TEST_QUESTIONS.length; x++)
-        TEST_QUESTIONS[x].selectedAnswer = '';
-        
+    resetSelectedAnswers();
     loadQuestion();
 };
 
@@ -60,8 +77,19 @@ document.getElementById("nextBtn").onclick = () =>{
 };
 
 
+function resetSelectedAnswers(){
+    for(let x = 0; x < TEST_QUESTIONS.length; x++){
+        if(TEST_QUESTIONS[x].type === QUESTION_TYPES.MULTIPLE_CHOICE)
+            TEST_QUESTIONS[x].selectedAnswer = '';
+        else if(TEST_QUESTIONS[x].type === QUESTION_TYPES.CHECK_BOX){
+            TEST_QUESTIONS[x].selectedAnswers = [];
+        }
+    }
+}
+
+
 //https://stackoverflow.com/questions/16505333/get-the-data-of-uploaded-file-in-javascript
-//Reads test data from user uploaded file and splits the test data in arrays containing objects with question data.
+//Reads test data from user uploaded file and splits the data into a test array consisting of an array of question objects.
 async function read(file) {
     let data = await file.text();
     let questions = data.split('[Q]'); //Split up data for each question into arrays.
@@ -74,15 +102,42 @@ async function read(file) {
         //If the question is missing too much information skip it.
         if(questionData.length < 3){continue}
             
-        let newQuestion = {
-            type: questionData.shift(),
-            question : questionData.shift(),
-            answers : questionData,
-            correctAnswer : questionData[0],
-            selectedAnswer : ''
-        };
+        let questionType = questionData.shift();
+        let questionText = questionData.shift()
         
-        TEST_QUESTIONS.push(newQuestion);
+        if(questionType == QUESTION_TYPES.MULTIPLE_CHOICE){
+            let newQuestion = {
+                type: questionType,
+                questionText : questionText,
+                answers : questionData,
+                correctAnswer : questionData[0],
+                selectedAnswer : ''
+            };
+            TEST_QUESTIONS.push(newQuestion);
+        }else if(questionType == QUESTION_TYPES.CHECK_BOX){
+            let correctAnswers = [];
+            let answers = [];
+            
+            //Remove `[C]` from correct answers and append the correct and extra answers
+            //to their respective arrays.
+            for(let x = 0; x < questionData.length; x++){
+                if(questionData[x].includes(`[C]`)){
+                    questionData[x] = questionData[x].replace(`[C]`, ``);
+                    correctAnswers.push(questionData[x]);
+                }
+                answers.push(questionData[x]);
+            }
+            
+            let newQuestion = {
+                type: questionType,
+                questionText : questionText,
+                answers : answers,
+                correctAnswers : correctAnswers,
+                selectedAnswers : []
+            };
+            TEST_QUESTIONS.push(newQuestion);
+        }
+        
     }
     return file.text();
 }
@@ -113,37 +168,57 @@ function randomizeAnswers(index){
 //Add question and answers to HTML page 
 function loadQuestion(){
     let QUESTION = TEST_QUESTIONS[questionIndex];
-    document.getElementById('questionTextField').innerHTML = QUESTION.question;
+    document.getElementById('questionTextField').innerHTML = QUESTION.questionText;
     
     //Clear out old answers.
     ANSWERS_DIV.replaceChildren([]);
     
-    for(let x = 0; x < QUESTION.answers.length; x++){
-        let isSelected = QUESTION.answers[x] == QUESTION.selectedAnswer;
-        let correctChoice = QUESTION.answers[x] == QUESTION.correctAnswer;
-        addRadioAnswer(x, QUESTION.answers[x], isSelected, correctChoice);
-    }
+    for(let x = 0; x < QUESTION.answers.length; x++){addAnswer(x, QUESTION.answers[x], QUESTION.type)}
+    
+    ANSWERS_DIV.style.height = QUESTION.answers.length * 10 + 'px';
 }
 
 //Creates a new input radio button and label containing the passed answer.
-function addRadioAnswer(index, answer, isSelected, correctChoice){
+function addAnswer(index, answer, type){
+    let currentQuestion = TEST_QUESTIONS[questionIndex];
     let newInputBtn = document.createElement("input");
     let newLabel = document.createElement("label");
     let answerContainer = document.createElement('div');
+    let correctChoice = false;
+    let isSelected = false;
     
     ANSWERS_DIV.appendChild(answerContainer);
     
     newInputBtn.classList.add('answerInput');
     newInputBtn.id = 'answer'+index;
-    newInputBtn.setAttribute('type', 'radio');
     newInputBtn.setAttribute('name', 'answerRadio');
     newInputBtn.setAttribute('value', answer);
     newInputBtn.setAttribute('flex', '1');
     newInputBtn.setAttribute('flex-basis', '50%');
     //Update the selected answer for the question.
-    newInputBtn.onclick = () =>{TEST_QUESTIONS[questionIndex].selectedAnswer = newInputBtn.value}
+    
+    print(`Type Info: ${type} ${QUESTION_TYPES.MULTIPLE_CHOICE} ${QUESTION_TYPES.CHECK_BOX}`);
+    if(type == QUESTION_TYPES.MULTIPLE_CHOICE){
+        newInputBtn.setAttribute('type', 'radio');
+        newInputBtn.onclick = () =>{currentQuestion.selectedAnswer = newInputBtn.value}
+        isSelected = currentQuestion.selectedAnswer === newInputBtn.value ? true : false;
+        newInputBtn.checked = isSelected;
+        correctChoice = currentQuestion.correctAnswer === newInputBtn.value;
+    }else if(type == QUESTION_TYPES.CHECK_BOX){
+        newInputBtn.setAttribute('type', 'checkbox');
+        newInputBtn.onclick = () =>{
+            selectedAnswers = currentQuestion.selectedAnswers;
+            if(newInputBtn.checked)
+                currentQuestion.selectedAnswers.push(newInputBtn.value);
+            else
+                currentQuestion.selectedAnswers = currentQuestion.selectedAnswers
+                    .filter(val => !val.includes(newInputBtn.value));
+        };
+        isSelected = currentQuestion.selectedAnswers.includes(newInputBtn.value) ? true : false;
+        newInputBtn.checked = isSelected;
+        correctChoice = currentQuestion.correctAnswers.includes(newInputBtn.value);
+    }
     newInputBtn.disabled = testSubmitted ? true : false;
-    newInputBtn.checked = isSelected ? true : false;
     answerContainer.appendChild(newInputBtn);
 
     newLabel.classList.add('answerLbl');
@@ -152,7 +227,7 @@ function addRadioAnswer(index, answer, isSelected, correctChoice){
     newLabel.setAttribute('flex', '1');
     newLabel.setAttribute('flex-basis', '50%');
     //Update the selected answer for the question.
-    newLabel.onclick = () =>{TEST_QUESTIONS[questionIndex].selectedAnswer = newLabel.textContent}
+    newLabel.onclick = () =>{currentQuestion.selectedAnswer = newLabel.textContent}
     //Highlight this answer green if the test has been submitted and this is the correct answer.
     if(testSubmitted && correctChoice)
         newLabel.classList.add('correctAnswer');
@@ -172,7 +247,7 @@ let testFS = document.getElementById('testFieldSet');
 let testElement = document.createElement("input");
 testElement.classList.add('answerInput');
 testElement.id = 'input1';
-testElement.setAttribute('type', 'radio');
+testElement.setAttribute('type', 'checkbox');
 testElement.setAttribute('name', 'answerInput');
 testElement.setAttribute('value', 'answer1');
 testElement.disabled = false;
